533b566f5a00c996c7ce56bdf174099cb64c7110
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-19 14:47:50 -0500

    user.js and thought.js updated

--- models/User.js
+++ models/User.js
@@ -1,6 +1,41 @@
 const { Schema, model } = require('mongoose');
 
 
+const UserSchema = new Schema( 
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        }, 
+        email: {
+            type: String,
+            required: true,
+            unique: true 
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: []
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
 
 
 
