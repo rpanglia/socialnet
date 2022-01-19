809bc51862808db7bf851c0866acdc1854f5f485
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-19 17:46:39 -0500

    routes for user and thoughts added

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+
+const { 
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought);
+
+// // /api/thoughts/<thoughtId>
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+
+// /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/:reactionId')
+    .post(addReaction)
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+module.exports = router; 
